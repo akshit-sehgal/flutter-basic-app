112bab54cf50d7e176f459a09b6057a2ecabdf1e
Akshit Sehgal <akshitsehgal95@gmail.com>
2019-11-12 20:47:28 +0530

    wiring up basic buttons and Layout Widgets

--- lib/main.dart
+++ lib/main.dart
@@ -6,12 +6,40 @@ import 'package:flutter/material.dart';
 void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
+  void answerQuestion() {
+    print('Something');
+  }
+
   @override
   Widget build(BuildContext context) {
+    List<String> questions = [
+      'What\'s your favourite color?',
+      'What\'s your favourite animals?'
+    ];
     return MaterialApp(
         home: Scaffold(
       appBar: AppBar(title: Text('Basic App')),
-      body: Text('This is the body text'),
+      body: Column(
+        children: [
+          Text('The question is: '),
+          RaisedButton(
+            child: Text('Answer 1'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 2'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 3'),
+            onPressed: answerQuestion,
+          ),
+          RaisedButton(
+            child: Text('Answer 4'),
+            onPressed: answerQuestion,
+          ),
+        ],
+      ),
     ));
   }
 }
