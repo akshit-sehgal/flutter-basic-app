a1e0b58cf0a48614ccabb7a26f24e5d3ec4ac818
Akshit Sehgal <akshitsehgal95@gmail.com>
2019-11-12 20:25:34 +0530

    Basic Page/AppBar/Body Setup

--- lib/main.dart
+++ lib/main.dart
@@ -1,12 +1,17 @@
 import 'package:flutter/material.dart';
 
-void main() {
-  runApp(new MyApp());
-}
+// void main() {
+//   runApp(new MyApp());
+// }
+void main() => runApp(MyApp());
 
 class MyApp extends StatelessWidget {
   @override
   Widget build(BuildContext context) {
-    return MaterialApp(home: Text('Hello'));
+    return MaterialApp(
+        home: Scaffold(
+      appBar: AppBar(title: Text('Basic App')),
+      body: Text('This is the body text'),
+    ));
   }
 }
